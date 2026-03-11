4564491b2ae9f19a060dedfb7c9df19864fd8405
semonhany <semonhany848@gmail.com>
2026-03-11 17:34:56 +0200

    re-add backend auth + frontend initial structure

---
+++ frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Register = () => {
+
+    return(
+        <h2>registration form</h2>
+    )
+};
+
+
+export default Register;
