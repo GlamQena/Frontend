94da01bd8b959748ba31643c41d93fcf6dbbfe52
semonhany <semonhany848@gmail.com>
2026-03-21 15:33:12 +0200

    edit resetPassword to use yup validations + test and solve logic errors in email verification + other random styles improvements

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as yup from "yup";
 
 const API_URL = "http://localhost:8080/auth";
 
@@ -11,3 +12,15 @@ export const registerUser = async (data) => {
     throw error.response.data;
   }
 };
+
+//yup shared schemas
+const passwordField= yup.string().required("كلمة المرور مطلوبة")
+      .min(8, "كلمة المرور يجب أن تكون 8 أحرف على الأقل")
+      .max(64, "كلمة المرور يجب ألا تتجاوز 64 حرف")
+      .matches(/[A-Z]/, "كلمة المرور يجب أن تحتوي على حرف كبير واحد على الأقل")
+      .matches(/[a-z]/, "كلمة المرور يجب أن تحتوي على حرف صغير واحد على الأقل")
+      .matches(/[0-9]/, "كلمة المرور يجب أن تحتوي على رقم واحد على الأقل")
+
+const emailField= yup.string().email().required("يرجى إدخال الإيميل").max(254, "البريد الإلكتروني يجب ألا يتجاوز 254 حرف");
+
+export {passwordField, emailField};
