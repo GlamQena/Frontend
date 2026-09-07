af6d86b1aa2053956821ac332c0583fc879b43e2
semonhany <semonhany848@gmail.com>
2026-09-07 22:55:46 +0300

    restore the /api proxy and use in stores service for check

--- src/services/stores.js
+++ src/services/stores.js
@@ -2,7 +2,7 @@ import { getCurrentUser } from "./users";
 
 const API_BASE_URL =
   process.env.EXPRESS_APP_API_URL || "https://glamqena-backend.vercel.app";
-const BASE_URL = `/stores`;
+const BASE_URL = `/api/stores`;
 
 // ─────────────────────────────────────────────
 // STORE HELPER
